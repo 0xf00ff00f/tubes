ae39035172a99270d84ab2329d21ea0579005fcc
fuse <fzorto@gmail.com>
2019-06-26 12:20:58 -0300

    some cleanup

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@
 #include "shader_program.h"
 #include "noise.h"
 #include "util.h"
+#include "rand.h"
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -15,27 +16,11 @@
 
 #include <algorithm>
 #include <iostream>
-#include <random>
 #include <memory>
 
 // #define DUMP_FRAMES
 
 #define PERTURB
-
-namespace
-{
-float rand_float()
-{
-    static std::mt19937 mt;
-    return static_cast<float>(mt() - mt.min())/(mt.max() - mt.min());
-}
-
-int rand_int()
-{
-    static std::mt19937 mt;
-    return mt();
-}
-}
 
 class tube_geometry
 {
@@ -140,11 +125,158 @@ private:
     geometry geometry_;
 };
 
-constexpr auto window_width = 512;
-constexpr auto window_height = 512;
-
-int main(int argc, char *argv[])
+class demo
 {
+public:
+    demo(int window_width, int window_height)
+        : window_width_(window_width)
+        , window_height_(window_height)
+    {
+        initialize_shader();
+        initialize_geometry();
+    }
+
+    void render_and_step(float dt)
+    {
+        const auto projection =
+            glm::perspective(glm::radians(45.0f), static_cast<float>(window_width_) / window_height_, 0.1f, 100.f);
+        const auto view_pos = glm::vec3(0, 0, 20.f - 1.f*cur_time_);
+        const auto view_up = glm::vec3(cosf(0.1f*cur_time_), sinf(0.1f*cur_time_), 0);
+        const auto view = glm::lookAt(view_pos, glm::vec3(0, 0, 0), view_up);
+
+        const auto model = glm::mat4(1.0f);
+        const auto mvp = projection * view * model;
+
+        glm::mat3 model_normal(model);
+        model_normal = glm::inverse(model_normal);
+        model_normal = glm::transpose(model_normal);
+
+        program_.bind();
+        program_.set_uniform(program_.uniform_location("mvp"), mvp);
+        program_.set_uniform(program_.uniform_location("normalMatrix"), model_normal);
+        program_.set_uniform(program_.uniform_location("modelMatrix"), model);
+
+        std::vector<glm::vec3> light_positions;
+        std::transform(lights_.begin(), lights_.end(), std::back_inserter(light_positions), [](auto &light) {
+            return light.tube->pos_at(light.t);
+        });
+        program_.set_uniform(program_.uniform_location("light_positions"), light_positions);
+        program_.set_uniform(program_.uniform_location("global_light"), glm::vec3(5, 7, 5));
+
+        for (auto &tube : tubes_)
+        {
+            tube->render();
+        }
+
+        cur_time_ += dt;
+        for (auto &light : lights_)
+        {
+            constexpr auto light_speed = 3.f;
+            light.t += light.direction ? -dt*light_speed : dt*light_speed;
+            if (light.t < 0 || light.t > 1)
+            {
+                light.tube = tubes_[rand_int() % tubes_.size()].get();
+                light.direction = rand_int() & 1;
+                light.t = light.direction ? 1 : 0;
+            }
+        }
+    }
+
+private:
+    void initialize_shader()
+    {
+        program_.add_shader(GL_VERTEX_SHADER, "shaders/tube.vert");
+        program_.add_shader(GL_FRAGMENT_SHADER, "shaders/tube.frag");
+        program_.link();
+    }
+
+    void initialize_geometry()
+    {
+        // grid
+
+        for (int i = 0; i < grid_layers; ++i)
+        {
+            for (int j = 0; j < grid_rows; ++j)
+            {
+                for (int k = 0; k < grid_cols; ++k)
+                {
+                    const auto z = -0.5f*grid_depth + i*cell_size;
+                    const auto y = -0.5f*grid_height + j*cell_size;
+                    const auto x = -0.5f*grid_width + k*cell_size;
+                    glm::vec3 p(x, y, z);
+#ifdef PERTURB
+                    p += cell_size*glm::vec3(rand_float() - 0.5f, rand_float() - 0.5f, rand_float() - 0.5f);
+#endif
+                    grid_[i][j][k] = p;
+                }
+            }
+        }
+
+        // tubes
+
+        for (int i = 0; i < grid_layers; ++i)
+        {
+            for (int j = 0; j < grid_rows; ++j)
+            {
+                for (int k = 0; k < grid_cols; ++k)
+                {
+                    if (i < grid_layers - 1)
+                        tubes_.emplace_back(new tube_geometry(grid_[i][j][k], grid_[i + 1][j][k]));
+
+                    if (j < grid_rows - 1)
+                        tubes_.emplace_back(new tube_geometry(grid_[i][j][k], grid_[i][j + 1][k]));
+
+                    if (k < grid_cols - 1)
+                        tubes_.emplace_back(new tube_geometry(grid_[i][j][k], grid_[i][j][k + 1]));
+                }
+            }
+        }
+
+        // lights
+
+        for (auto &light : lights_)
+        {
+            light.tube = tubes_[rand_int() % tubes_.size()].get();
+            light.t = rand_float();
+            light.direction = rand_int() & 1;
+        }
+    }
+
+    int window_width_;
+    int window_height_;
+
+    float cur_time_ = 0.f;
+
+    shader_program program_;
+
+    static constexpr auto grid_cols = 5;
+    static constexpr auto grid_rows = 5;
+    static constexpr auto grid_layers = 5;
+
+    static constexpr auto cell_size = 5.f;
+
+    static constexpr auto grid_width = cell_size * (grid_cols - 1);
+    static constexpr auto grid_height = cell_size * (grid_rows - 1);
+    static constexpr auto grid_depth = cell_size * (grid_layers - 1);
+
+    std::array<std::array<std::array<glm::vec3, grid_cols>, grid_rows>, grid_layers> grid_;
+    std::vector<std::unique_ptr<tube_geometry>> tubes_;
+
+    struct light
+    {
+        tube_geometry *tube;
+        float t;
+        bool direction;
+    };
+    static constexpr int num_lights = 17;
+    std::array<light, num_lights> lights_;
+};
+
+int main()
+{
+    constexpr auto window_width = 512;
+    constexpr auto window_height = 512;
+
     if (!glfwInit())
         panic("glfwInit failed\n");
 
@@ -168,14 +300,6 @@ int main(int argc, char *argv[])
             glfwSetWindowShouldClose(window, GL_TRUE);
     });
 
-    const auto projection =
-        glm::perspective(glm::radians(45.0f), static_cast<float>(window_width) / window_height, 0.1f, 100.f);
-
-    shader_program program;
-    program.add_shader(GL_VERTEX_SHADER, "shaders/tube.vert");
-    program.add_shader(GL_FRAGMENT_SHADER, "shaders/tube.frag");
-    program.link();
-
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
     glEnable(GL_MULTISAMPLE);
@@ -184,80 +308,9 @@ int main(int argc, char *argv[])
     constexpr auto total_frames = 150;
     auto frame_num = 0;
 #endif
-    float cur_time = 0.0;
 
     {
-    constexpr auto grid_cols = 5;
-    constexpr auto grid_rows = 5;
-    constexpr auto grid_layers = 5;
-
-    constexpr auto cell_size = 5.;
-
-    constexpr auto grid_width = cell_size * (grid_cols - 1);
-    constexpr auto grid_height = cell_size * (grid_rows - 1);
-    constexpr auto grid_depth = cell_size * (grid_layers - 1);
-
-    std::vector<std::vector<std::vector<glm::vec3>>> grid(grid_layers);
-    for (auto &layer : grid)
-    {
-        layer.resize(grid_rows);
-        for (auto &row : layer)
-            row.resize(grid_cols);
-    }
-
-    for (int i = 0; i < grid_layers; ++i)
-    {
-        for (int j = 0; j < grid_rows; ++j)
-        {
-            for (int k = 0; k < grid_cols; ++k)
-            {
-#ifdef PERTURB
-                const auto z = -0.5*grid_depth + (i + (rand_float() - 0.5f))*cell_size;
-                const auto y = -0.5*grid_height + (j + (rand_float() - 0.5f))*cell_size;
-                const auto x = -0.5*grid_width + (k + (rand_float() - 0.5f))*cell_size;
-#else
-                const auto z = -0.5*grid_depth + i*cell_size;
-                const auto y = -0.5*grid_height + j*cell_size;
-                const auto x = -0.5*grid_width + k*cell_size;
-#endif
-                grid[i][j][k] = glm::vec3(x, y, z);
-            }
-        }
-    }
-
-    std::vector<std::unique_ptr<tube_geometry>> tubes;
-    for (int i = 0; i < grid_layers; ++i)
-    {
-        for (int j = 0; j < grid_rows; ++j)
-        {
-            for (int k = 0; k < grid_cols; ++k)
-            {
-                if (i < grid_layers - 1)
-                    tubes.emplace_back(new tube_geometry(grid[i][j][k], grid[i + 1][j][k]));
-
-                if (j < grid_rows - 1)
-                    tubes.emplace_back(new tube_geometry(grid[i][j][k], grid[i][j + 1][k]));
-
-                if (k < grid_cols - 1)
-                    tubes.emplace_back(new tube_geometry(grid[i][j][k], grid[i][j][k + 1]));
-            }
-        }
-    }
-
-    constexpr int num_lights = 17;
-    struct light
-    {
-        tube_geometry *tube;
-        float t;
-        bool direction;
-    };
-    std::array<light, num_lights> lights;
-    for (auto &light : lights)
-    {
-        light.tube = tubes[rand_int() % tubes.size()].get();
-        light.t = rand_float();
-        light.direction = rand_int() & 1;
-    }
+    demo d(window_width, window_height);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -266,56 +319,11 @@ int main(int argc, char *argv[])
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 #ifdef DUMP_FRAMES
-        float angle = 1.5f * sinf(static_cast<float>(frame_num) * 2.0f * M_PI / total_frames);
+        const auto dt = 1.0f / 40;
 #else
-        float angle = 1.5f * sinf(cur_time);
+        const auto dt = 1.0f / 60;
 #endif
-
-        const auto view_pos = glm::vec3(0, 0, 20.f - 1.f*cur_time);
-        const auto view_up = glm::vec3(cosf(0.1f*cur_time), sinf(0.1f*cur_time), 0);
-        const auto view = glm::lookAt(view_pos, glm::vec3(0, 0, 0), view_up);
-
-        // const auto model = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0, 1, 0));
-        const auto model = glm::mat4(1.0f);
-        const auto mvp = projection * view * model;
-
-        glm::mat3 model_normal(model);
-        model_normal = glm::inverse(model_normal);
-        model_normal = glm::transpose(model_normal);
-
-        program.bind();
-        program.set_uniform(program.uniform_location("mvp"), mvp);
-        program.set_uniform(program.uniform_location("normalMatrix"), model_normal);
-        program.set_uniform(program.uniform_location("modelMatrix"), model);
-
-        glm::vec3 light_position = tubes.front()->pos_at(glm::fract(cur_time));
-
-        std::vector<glm::vec3> light_positions;
-        std::transform(lights.begin(), lights.end(), std::back_inserter(light_positions), [](auto &light) {
-            return light.tube->pos_at(light.t);
-        });
-        program.set_uniform(program.uniform_location("light_positions"), light_positions);
-        program.set_uniform(program.uniform_location("global_light"), glm::vec3(5, 7, 5));
-
-        for (auto &tube : tubes)
-        {
-            tube->render();
-        }
-
-        for (auto &light : lights)
-        {
-            constexpr float dt = 0.05;
-            if (light.direction)
-                light.t -= dt;
-            else
-                light.t += dt;
-            if (light.t < 0 || light.t > 1)
-            {
-                light.tube = tubes[rand_int() % tubes.size()].get();
-                light.direction = rand_int() & 1;
-                light.t = light.direction ? 1 : 0;
-            }
-        }
+        d.render_and_step(dt);
 
 #ifdef DUMP_FRAMES
         char path[80];
@@ -324,9 +332,6 @@ int main(int argc, char *argv[])
 
         if (++frame_num == total_frames)
             break;
-        cur_time += 1.0f / 40;
-#else
-        cur_time += 1.0f / 60;
 #endif
 
         glfwSwapBuffers(window);
